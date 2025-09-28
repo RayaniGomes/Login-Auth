37a643abb803af850f68205aed4cf1697cd19deb
RayaniGomes <rayanigomesrn100@gmail.com>
2025-09-28 13:45:17 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,11 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/",
   server: {
     host: "::",
     port: 5173,
+    historyApiFallback: true,
   },
   plugins: [react()],
   resolve: {
